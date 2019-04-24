608672e7172710186c8c95337c86ec3924098f16
Dorin Keshales <dorin140895@gmail.com>
2019-04-25 02:58:18 +0300

    Small update of AutoPilot (ViewModel and xaml).

---
+++ FlightSimulator/ViewModels/AutoPilotViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FlightSimulator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotViewModel : BaseNotify
+    {
+        private AutoPilotModel model;
+
+        public AutoPilotViewModel(AutoPilotModel model)
+        {
+            this.model = model;
+        }
+
+    }
+}
